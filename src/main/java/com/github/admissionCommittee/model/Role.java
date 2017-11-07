1a5008c6ebe9420b2796f685e985c32fec142d66
timaliberdov <timur.aliberdov@yandex.ru>
2017-11-07 18:39:56 +0300

    Changed Role type from class to enum

--- src/main/java/com/github/admissionCommittee/model/Role.java
+++ src/main/java/com/github/admissionCommittee/model/Role.java
@@ -1,36 +1,6 @@
 package com.github.admissionCommittee.model;
 
-import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
-//@Cache(usage = CacheConcurrencyStrategy.)
-
-@Entity
-@Table(name = "roles")/*, uniqueConstraints = {
-        @UniqueConstraint(columnNames =
-                "role", name = "roles_unique_role")
-})*/
-public class Role extends NamedEntity {
-
-    @OneToMany(mappedBy = "role")
-    private Set<User> users = new HashSet<>();
-
-    public Role() {
-    }
-
-    public Role(User role) {
-        this(role.getName());
-    }
-
-    public Role(String name) {
-        super(name);
-    }
-
-    @Override
-    public String toString() {
-        return "Role{" +
-                "id=" + getId() +
-                ", name=" + name +
-                +'}';
-    }
+public enum Role {
+    ENROLLEE,
+    ADMIN
 }
