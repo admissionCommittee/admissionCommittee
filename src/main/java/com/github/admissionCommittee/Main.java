8098abcadafebfb4205a5aeae0c8362b2b073ef7
timaliberdov <timur.aliberdov@yandex.ru>
2017-11-07 16:32:03 +0300

    Fixed hibernate config and gradle dependencies

--- src/main/java/com/github/admissionCommittee/Main.java
+++ src/main/java/com/github/admissionCommittee/Main.java
@@ -7,6 +7,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import java.util.List;
+
 
 /**
  * Created by Admin on 07.11.2017.
@@ -18,21 +20,30 @@ public class Main {
         session.beginTransaction();
         Role role = new Role("admin");
         session.save(role);
+        final Role role1 = new Role("user1");
+        session.save(role1);
 
         User user = new User("firstUser",role);
 
 
 
         session.save(user);
+        session.save(new User("second", role));
+        session.save(new User("third", role1));
         Transaction transaction= session.getTransaction();
         //transaction.rollback();
         transaction.commit();
 
         session.beginTransaction();
-        User load = session.load(User.class, 0);
-        Role loadedRole = session.load(Role.class, 0);
-        System.out.println(user.getName());
-        System.out.println(loadedRole.getName());
+        //User load = session.load(User.class, 0);
+        //Role loadedRole = session.load(Role.class, 0);
+        //System.out.println(user.getName());
+        //System.out.println(loadedRole.getName());
+
+        List userResult = session.createQuery("from User").list();
+        userResult.forEach(u -> System.out.println("user: " + ((User)u).getName() + " " + ((User)u).getId()));
+        List roleResult = session.createQuery("from Role").list();
+        roleResult.forEach(r -> System.out.println("role: " + ((Role)r).getName() + " " + ((Role)r).getId()));
 
         /*CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
         CriteriaQuery<User> cq = cb.createQuery(Pet.class);
