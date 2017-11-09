d23ed90554f1290216f3f2d1a7e13ca28f8c69ad
timaliberdov <timur.aliberdov@yandex.ru>
2017-11-09 10:36:21 +0300

    New entities

---
+++ src/main/java/com/github/admissionCommittee/core/AbstractEntity.java
@@ -0,0 +1,28 @@
+package com.github.admissionCommittee.core;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public abstract class AbstractEntity implements Serializable {
+
+    private static final long serialVersionUID = 42L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
