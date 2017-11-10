6a7570232cc36d1deab5d3be96cb4a898e044272
timaliberdov <timur.aliberdov@yandex.ru>
2017-11-10 17:08:21 +0300

    added @Data annotation for AbstractEntity (needed equals and hashcode)

--- src/main/java/com/github/admissionCommittee/model/AbstractEntity.java
+++ src/main/java/com/github/admissionCommittee/model/AbstractEntity.java
@@ -1,7 +1,6 @@
 package com.github.admissionCommittee.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
@@ -9,12 +8,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+@Data
 @MappedSuperclass
 public abstract class AbstractEntity {
 
     @Id
-    @Getter
-    @Setter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true, nullable = false)
     private Long id;
